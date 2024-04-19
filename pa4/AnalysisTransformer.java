50ea96c7b1ca818e0599ba13c424f0b70b06b646
Virendra Kabra <virendrakabra14@gmail.com>
2024-04-19 17:45:38 +0530

    initial exploration

--- pa4/AnalysisTransformer.java
+++ pa4/AnalysisTransformer.java
@@ -5,16 +5,31 @@ import soot.jimple.AssignStmt;
 import soot.jimple.AddExpr;
 import soot.jimple.MulExpr;
 import soot.jimple.Jimple;
+// import soot.toolkits.scalar.;
+import soot.jimple.toolkits.annotation.nullcheck.NullPointerColorer; // stmt.getTag("NullCheckTag")
+import soot.tagkit.Tag;
+import soot.toolkits.graph.CompleteUnitGraph;
+import soot.jimple.toolkits.annotation.nullcheck.NullnessAnalysis;
 
 public class AnalysisTransformer extends BodyTransformer {
     @Override
-    protected void internalTransform(Body body, String phaseName, Map<String, String> options) {
+    synchronized protected void internalTransform(Body body, String phaseName, Map<String, String> options) {
+        boolean DEBUG = true;
+
+        NullnessAnalysis na = new NullnessAnalysis(new CompleteUnitGraph(body));
+
         // Iterate over all units (instructions) in the method body
         PatchingChain<Unit> units = body.getUnits();
         // Iterate over instructions and replace iadd with imul
         Iterator<Unit> unitIt = units.snapshotIterator();
-        while (unitIt.hasNext()) {
+        while (unitIt.hasNext()) {  // TODO: go to successor(s)?
+                                    // TODO: worklist?
             Unit unit = unitIt.next();
+            if(DEBUG) { System.out.println(unit + ": "); }
+            System.out.println("    " + na.getFlowBefore(unit));
+            for(Tag t: unit.getTags()) {
+                System.out.println("    " + t.getName() + "=" + t);
+            }
             if (unit instanceof Stmt) {
                 Stmt stmt = (Stmt) unit;
                 // Check if it’s an iadd operation
